80491aa595a72af3a74004a1e82c9478c9e755a9
irinaamsn <ira.myasnikova.02@list.ru>
2023-10-08 22:18:27 +0300

    Внесены изменения в папке exceptions

--- src/main/java/com/gradle/boot/fintech/exceptions/RegionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gradle.boot.fintech.exceptions;
-
-public class RegionNotFoundException extends RuntimeException{
-    @Override
-    public String getMessage() {
-        return "Region not found";
-    }
-}
--- src/main/java/com/gradle/boot/fintech/exceptions/TemperatureNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.gradle.boot.fintech.exceptions;
-
-public class TemperatureNotFoundException extends RuntimeException{
-
-    @Override
-    public String getMessage() {
-        return "Temperature not found";
-    }
-}
