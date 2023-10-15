38ef013d2c2738f153c45f26134a6722c528be9f
irinaamsn <ira.myasnikova.02@list.ru>
2023-10-15 23:35:15 +0300

    Изменен config

---
+++ src/main/java/com/gradle/boot/fintech/config/SpringJdbcConfig.java
@@ -0,0 +1,21 @@
+package com.gradle.boot.fintech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringJdbcConfig {
+    private final DataSource dataSource;
+
+    public SpringJdbcConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+}
