67a387fdbb4cb2def2cb553196a8351e7a5103a6
irinaamsn <ira.myasnikova.02@list.ru>
2023-10-15 22:39:27 +0300

    Изменен WeatherServiceImpl

--- src/main/java/com/gradle/boot/fintech/services/WeatherService.java
+++ src/main/java/com/gradle/boot/fintech/services/WeatherService.java
@@ -3,8 +3,8 @@ package com.gradle.boot.fintech.services;
 import com.gradle.boot.fintech.models.Weather;
 
 public interface WeatherService {
-    void save(int regionId, Weather weather);
-    void update(int regionId, Weather weather);
-    Double getTempByRegionId(int regionId);
-    void delete(int regionId) ;
+    void save(int regionCode, Weather weather);
+    void update(int regionCode, Weather weather);
+    Double getTempByRegionId(int regionCode);
+    void delete(int regionCode) ;
 }
