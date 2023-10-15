a3c024e84be24b8c41752405eb86ab5e46e361ad
irinaamsn <ira.myasnikova.02@list.ru>
2023-10-15 22:43:23 +0300

    Изменен класс погоды, добавлены классы регион и тип погоды

--- src/main/java/com/gradle/boot/fintech/models/Weather.java
+++ src/main/java/com/gradle/boot/fintech/models/Weather.java
@@ -1,32 +1,56 @@
 package com.gradle.boot.fintech.models;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Weather")
 public class Weather {
-    @Schema(description = "Region ID")
-    private int regionId;
-    @Schema(description = "Name of the region", example = "Moscow oblast")
-    private String regionName;
-    @Schema(description = "Temperature")
-    private double valueTemp;
-    @Schema(description = "Date and time")
-    private LocalDateTime dateTime;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Override
-    public String toString() {
-        return "Weather{" +
-                "id=" + regionId +
-                ",region='" + regionName + '\'' +
-                ",temperature=" + valueTemp +
-                ",dateTime=" + dateTime +
-                '}';
+    @Column(name = "regionCode", nullable = false)
+    private int regionCode;
+
+    @Column(name = "regionName", nullable = false)
+    private String regionName;
+
+    @Column(name = "temperature", nullable = false)
+    private Double temperature;
+
+    @Column(name = "typeName", nullable = false)
+    private String typeName;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id", referencedColumnName = "id")
+    private Region region;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id", referencedColumnName = "id")
+    private WeatherType weatherType;
+
+    public Weather(int regionCode, String regionName, Double temperature, String typeName, LocalDate date, LocalTime time) {
+        this.regionCode = regionCode;
+        this.regionName = regionName;
+        this.temperature = temperature;
+        this.typeName = typeName;
+        this.date = date;
+        this.time = time;
     }
 }
